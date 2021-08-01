b9e9f6d6c85bf139cb9de694d743487502e97726
smapira <smapira@routeflags.com>
2021-08-01 09:03:42 +0900

    Remove storyboard

--- Tried-using-the-MessageKit/AppDelegate.swift
+++ Tried-using-the-MessageKit/AppDelegate.swift
@@ -11,10 +11,14 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
-	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-		// Override point for customization after application launch.
+	var window: UIWindow?
+	
+	func application(_ application: UIApplication,
+					 didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		let window = UIWindow(frame: UIScreen.main.bounds)
+		window.rootViewController = ViewController()
+		window.makeKeyAndVisible()
+		self.window = window
 		return true
 	}
 
